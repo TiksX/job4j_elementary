14b913e2f31b6885b7fe500a84d63dab22654976
TiksX <tikssanek@gmai.com>
2025-02-13 21:30:27 +0300

    test

--- src/test/java/ru/job4j/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/ru/job4j/converter/ConverterTest.java
@@ -9,7 +9,7 @@ class ConverterTest {
     void whenConvert140RblThen2Euro() {
         float input = 140;
         float expected = 2;
-        float output = (float)Converter.rubleToEuro(input);
+        float output = (float) Converter.rubleToEuro(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
