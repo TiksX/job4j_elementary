b9d4f26666920b43594af8d332fea3bbc78ab0ed
TiksX <tikssanek@gmai.com>
2025-02-15 11:33:52 +0300

    Заголовок в файле java [#505272 #532506]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Info {
         public static void main(String[] args) {
-            System.out.println("15.02.2025");
+            System.out.println("16.02.2025");
         }
     }
