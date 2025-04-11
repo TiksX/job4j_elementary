6ac49c37e515a58c3ddd01a0d20f6d2f10ab2451
TiksX <tikssanek@gmai.com>
2025-04-11 19:06:19 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -3,6 +3,9 @@ package ru.job4j.loop;
 public class Factorial {
 
     public static int calculate(int number) {
+        if (number < 2) {
+            throw new IllegalArgumentException("Start should be less than finish.");
+        }
         int result = 1;
         for (int i = 2; i <= number; i++) {
             result *= i;
